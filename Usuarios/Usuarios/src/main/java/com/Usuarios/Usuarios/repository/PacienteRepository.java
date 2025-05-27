8ad397d478a5c59fede032c0bffe777023b7555e
Raúl Bustamante <rau.bustamante@duocuc.cl>
2025-05-26 20:46:26 -0400

    Add files via upload
    MicroServicio de Francisco

---
+++ Usuarios/Usuarios/src/main/java/com/Usuarios/Usuarios/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.Usuarios.Usuarios.repository;
+
+import com.Usuarios.Usuarios.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
